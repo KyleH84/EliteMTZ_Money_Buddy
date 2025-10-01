0b8607010bdbd5748d1abb4f96ad417a091e6373
Kyle Hansen <kyle.e.hansen@hotmail.com>
2025-10-01 13:31:48 -0600

    AstroLotto: utilities.oracle_data bridge; BreakoutBuddy: About tab syntax fix

--- AstroLotto/programs/utilities/__init__.py
+++ AstroLotto/programs/utilities/__init__.py
@@ -1,7 +1,1 @@
-# Bridge so imports like `from utilities.x import ...` resolve to modules.utilities.x
-import importlib, sys
-def __getattr__(name: str):
-    modname = f"modules.utilities.{name}"
-    m = importlib.import_module(modname)
-    sys.modules[f"{__name__}.{name}"] = m
-    return m
+# package bridge for utilities
